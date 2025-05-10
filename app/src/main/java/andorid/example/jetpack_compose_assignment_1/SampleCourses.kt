4d06d7e1d6013601d17bae9faaefa2445d74ace0
Dawit <dawitmisgna4@gmail.com>
2025-05-10 10:51:53 +0300

    Jetpack Compose

---
+++ app/src/main/java/andorid/example/jetpack_compose_assignment_1/SampleCourses.kt
@@ -0,0 +1,14 @@
+package andorid.example.jetpack_compose_assignment_1
+
+val sampleCourses = listOf(
+    CourseModel("Mobile Development", "CS350", 3,  "Introduction to mobile application development for iOS and Android platforms, covering UI design, data persistence, and mobile-specific features."),
+    CourseModel("Software Engineering", "SE202", 3, "Covers lifecycle models, requirements, design, testing."),
+    CourseModel("Data Structures", "CS201", 4, "Introduction to arrays, stacks, queues, trees."),
+    CourseModel("Operating Systems", "CS301", 3, "Concepts of process, memory, file, and device management."),
+    CourseModel( "Cybersecurity","CS460", 3,"Principles of cybersecurity, including threat modeling, cryptography, network security, and ethical hacking."),
+    CourseModel("Fundamentals of AI","CS370",3,"Introduction to artificial intelligence, covering search algorithms, knowledge representation, machine learning, and AI applications."),
+    CourseModel("Cybersecurity Lab","CS461",1,"Hands-on experience with cybersecurity tools and techniques, including penetration testing, vulnerability analysis, and incident response."
+    )
+
+
+)
